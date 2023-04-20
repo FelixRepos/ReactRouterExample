5f91ecfca04a0d0d4b173b9effbfa325b60127bf
FelixRepos <goebs@outlook.de>
2023-04-20 11:32:17 +0200

    delete not needed files

--- src/routes/ProductOverview.jsx
+++ src/routes/ProductOverview.jsx
@@ -1,8 +1,6 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function ProductOverview() {
-const catPictures = useLoaderData();
-console.log(catPictures)
 
   return (
     <>
